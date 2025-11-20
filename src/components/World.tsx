94dabda410e1c03387eaff118f9ea2518f2994b3
sawanori <snp.inc.info@gmail.com>
2025-11-20 17:13:58 +0900

    3nd commit

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -6,6 +6,7 @@ import { Lantern } from './models/Lantern'
 import { TableSet } from './models/TableSet'
 import { Noren } from './models/Noren'
 import { SakeBottle } from './models/SakeBottle'
+import { Okami } from './models/Okami'
 
 const Zone = ({ position, size, id, label }: { position: [number, number, number], size: [number, number, number], id: string, label: string }) => {
     const setActiveZone = useStore((state) => state.setActiveZone)
@@ -68,11 +69,13 @@ export const World = () => {
             <Zone position={[10, 0, 10]} size={[2, 2, 2]} id="seats" label={SECTIONS.find(s => s.id === 'seats')?.title || ''} />
             <Zone position={[0, 0, 15]} size={[2, 2, 2]} id="location" label={SECTIONS.find(s => s.id === 'location')?.title || ''} />
 
+
+
             {/* Decor - Counter Area */}
             <RigidBody type="fixed">
                 {/* Main Counter */}
-                <mesh position={[-10, 1, -10]}>
-                    <boxGeometry args={[15, 2, 4]} />
+                <mesh position={[-10, 0.5, -10]}>
+                    <boxGeometry args={[15, 1, 4]} />
                     <meshStandardMaterial color="#5c3a21" />
                 </mesh>
                 {/* Kitchen Back Wall / Shelf */}
@@ -82,15 +85,21 @@ export const World = () => {
                 </mesh>
             </RigidBody>
 
+            {/* Decor - Okami (Proprietress) */}
+            <group position={[-12, 0, -14]} rotation={[0, Math.PI / 4, 0]}>
+                <Okami position={[0, 0, 0]} />
+                <pointLight position={[0, 2, 1]} intensity={1} distance={3} color="#ffaa00" />
+            </group>
+
             {/* Decor - Noren (Entrance) */}
             <Noren position={[0, 2.5, 20]} />
 
             {/* Decor - Sake Bottles */}
-            <SakeBottle position={[-10, 2.2, -10]} color="#4a90e2" />
-            <SakeBottle position={[-11, 2.2, -10]} color="#e24a4a" />
-            <SakeBottle position={[-9, 2.2, -10]} color="#4ae290" />
-            <SakeBottle position={[-12, 2.2, -10]} />
-            <SakeBottle position={[-8, 2.2, -10]} />
+            <SakeBottle position={[-10, 1.0, -10]} color="#4a90e2" />
+            <SakeBottle position={[-11, 1.0, -10]} color="#e24a4a" />
+            <SakeBottle position={[-9, 1.0, -10]} color="#4ae290" />
+            <SakeBottle position={[-12, 1.0, -10]} />
+            <SakeBottle position={[-8, 1.0, -10]} />
 
             {/* Shelf Bottles */}
             <SakeBottle position={[-14.5, 3, -12]} color="#e2e24a" />
